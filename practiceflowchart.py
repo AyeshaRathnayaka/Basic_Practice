a9713fe4344da70d5eb9223d6e1c78a726ed9477
chamod0 <ayesharathnayaka35@gmail.com>
2025-09-03 10:51:15 +0530

    basic code

--- practiceflowchart.py
+++ practiceflowchart.py
@@ -31,3 +31,26 @@ else:
 Celcius= float(input("Celsius temperature :"))
 Fahrenheit=float(Celcius*9/5+32)
 print("Fahrenheit is : " + str(Fahrenheit))
+
+
+
+
+
+
+#find the largest among three numbers
+No1=input("First Number : ")
+No2=input("Second Number : ")
+No3=input("Third Number: ")
+
+if No1>No2>No3 :
+    print("Largest Number is "+ str(No1))
+elif No1<No2<No3 :
+    print("Largest Number is "+ str(No3))
+elif No2<No3<No1 :
+    print("Largest Number is "+ str(No1))
+elif No2>No3>No1 :
+    print("Largest Number is " + str(No2))
+elif No3<No1<No2 :
+    print("Largest Number is " + str(No2))
+else:
+    print("Largest Number is "+ str(No3))
